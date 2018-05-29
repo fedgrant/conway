8e328b1cd191b0c3d7c9ce0adfea9706d4126fcc
mike <mike.local>
2018-05-29 14:40:33 -0400

    rust stuff

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let state = vec![ vec![0i8; 10]; 10];
+    // println!("state {:?}", state);
+
+    for i in &mut state.enumerate() {
+        // for j in state[i].enumerate() {
+            println!("{:?}", state[i])
+        // }
+    }
 }
